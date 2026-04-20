dac097c825182b29e066217e2af455fa31ab41a2
Henoik <henoik.willot@gmail.com>
2026-04-20 18:07:37 +0200

    Test with animal dataset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using NaiveCredalClassifier
+using MLJBase
 using Test
+using CSV
+using DataFrames
 
-@testset "NaiveCredalClassifier.jl" begin
-    # Write your tests here.
-end
+T = CSV.read("./test/animal_dataset.csv", DataFrame)
+T = coerce(T, :hair => Multiclass, :tail => Multiclass, :ear => Multiclass, :animal => Multiclass)
+y, X = unpack(T, ==(:animal))
+
+Xnew = DataFrame(ear=["Long"], tail=["Short"], hair=["Long"])
+Xnew = coerce(Xnew, :hair => Multiclass, :tail => Multiclass, :ear => Multiclass)
+levels!(Xnew.ear, ["Long", "Medium", "Short"])
+levels!(Xnew.hair, ["Long", "Medium", "Short"])
+levels!(Xnew.tail, ["Long", "Medium", "Short"])
+#Xnew =  Xnew[1,:]
+
+ncc = NaiveCredalClassifier.NCClassifier()
+mach = machine(ncc, X, y)
+fit!(mach)
+println(predict(mach, Xnew))
+println(fitted_params(mach))
