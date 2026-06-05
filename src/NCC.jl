5f8dea176d26029fdc37c58c37c3e0348dfcd2d8
Henoik <henoik.willot@gmail.com>
2026-06-05 17:06:01 +0200

    Using Tables interface

--- src/NCC.jl
+++ src/NCC.jl
@@ -1,6 +1,6 @@
 using CategoricalArrays
 using MLJModelInterface
-import DataFrames: DataFrame
+using Tables
 const MMI = MLJModelInterface
 
 MMI.@mlj_model mutable struct NCClassifier <: MMI.Deterministic
@@ -10,33 +10,29 @@ MMI.@mlj_model mutable struct NCClassifier <: MMI.Deterministic
 end
 
 function MMI.fit(m::NCClassifier, verbosity::Int, X, y)
-    Xmatrix = MMI.matrix(X)
+    #Xmatrix = MMI.matrix(X)
     decode_y = MMI.decoder(y[1])
-    decode_x = tuple(collect(MMI.decoder(x) for x in Xmatrix[1,:])...)
-    if X isa DataFrame
-        names_x = names(X[1,:])
-    elseif X isa NamedTuple
-        names_x = collect(keys(X))
-    else
-        names_x = collect(1:size(Xmatrix,2))
-    end
-    n_y = length(levels(y))
-    n_x = [length(levels(Xmatrix[:,x])) for x in range(1,size(Xmatrix,2))]
+    names_x = Tables.columnnames(X)
+    decode_x = tuple(collect(MMI.decoder(Tables.getcolumn(X,n)[1]) for n in names_x)...)
     y_l = MMI.int(y)
-    x_l = MMI.int.(Xmatrix)
-    cond_count = [zeros(Int64, (n_x[x], n_y)) for x in range(1,size(Xmatrix,2))]
+    x_l = reduce(hcat, MMI.int.(Tables.getcolumn(X,n)) for n in names_x)
+
+    n_y = length(levels(y))
+    n_x = [length(levels(decode_x[x])) for x in range(1,size(x_l,2))]
+
+    cond_count = [zeros(Int64, (n_x[x], n_y)) for x in range(1,size(x_l,2))]
     y_count = zeros(Int64, n_y)
-    for row in range(1,size(Xmatrix,1))
+    for row in range(1,size(x_l,1))
         y_count[y_l[row]] += 1
-        for col in range(1,size(Xmatrix,2))
+        for col in range(1,size(x_l,2))
             cond_count[col][x_l[row,col],y_l[row]] += 1
         end
     end
 
-    cond_prob = [zeros(Float64, (n_x[x], n_y, 2)) for x in range(1,size(Xmatrix,2))]
+    cond_prob = [zeros(Float64, (n_x[x], n_y, 2)) for x in range(1,size(x_l,2))]
     y_prob = zeros(Float64, (n_y,2))
 
-    for col in range(1,size(Xmatrix,2)), val in range(1, n_x[col]), y_cond in range(1,n_y)
+    for col in range(1,size(x_l,2)), val in range(1, n_x[col]), y_cond in range(1,n_y)
         cond_prob[col][val, y_cond, 1] = (1-m.epsilon) * cond_count[col][val,y_cond] * (1/(sum(cond_count[col][:,y_cond])+m.s)) + m.epsilon / n_x[col]
         cond_prob[col][val, y_cond, 2] = (1-m.epsilon) * (cond_count[col][val,y_cond] + m.s) * (1/(sum(cond_count[col][:,y_cond])+m.s)) + m.epsilon / n_x[col]
     end
@@ -51,20 +47,21 @@ function MMI.fit(m::NCClassifier, verbosity::Int, X, y)
 end
 
 function MMI.predict(m::NCClassifier, fitresult, Xnew)
-    (_, _, decode_y, _, _, _, _) = fitresult
+    (_, _, decode_y, _, names_x, _, _) = fitresult
 	y_hat = Vector{CategoricalValue}[]
-    Xmatrix = MMI.matrix(Xnew)
+    Xmatrix = reduce(hcat, MMI.int.(Tables.getcolumn(Xnew,n)) for n in Tables.columnnames(Xnew))
 	for x in eachrow(Xmatrix)
-		push!(y_hat, decode_y.(RCB.predict(m.decisionRule, fitresult, [Int64(MMI.int(x[i])) for i in range(1, size(x, 1))])))
+		push!(y_hat, decode_y.(RCB.predict(m.decisionRule, fitresult, x)))
 	end
 	return y_hat
 end
 
 function MMI.fitted_params(m::NCClassifier, fitresult)
     cond_prob, y_prob, decode_y, decode_x, names_x, cond_count, y_count = fitresult
-    return (; x_probas=(;zip(Symbol.(names_x), [(; zip(Symbol.(decode_x[x].classes), [Dict("$x_val|$y_val" => [cond_prob[x][x_v, y_v,1], cond_prob[x][x_v,y_v,2]] for (y_v,y_val) in enumerate(decode_y.classes)) for (x_v, x_val) in enumerate(decode_x[x].classes)])...) for x in 1:length(names_x)])...), 
-        x_counts=(;zip(Symbol.(names_x), [(; zip(Symbol.(decode_x[x].classes), [Dict("$x_val|$y_val" => cond_count[x][x_v, y_v] for (y_v,y_val) in enumerate(decode_y.classes)) for (x_v, x_val) in enumerate(decode_x[x].classes)])...) for x in 1:length(names_x)])...), 
-        y=(;zip(Symbol.(decode_y.classes), [[y_prob[y,1], y_prob[y,2]] for y in 1:size(y_prob,1)])...), 
+    return (; 
+        x_probas=(;zip(Symbol.(names_x), [(; zip(Symbol.(decode_x[x].classes), [Dict("$x_val|$y_val" => [cond_prob[x][x_v, y_v,1], cond_prob[x][x_v,y_v,2]] for (y_v,y_val) in enumerate(decode_y.classes)) for (x_v, x_val) in enumerate(decode_x[x].classes)])...) for x in 1:length(names_x)])...),
+        x_counts=(;zip(Symbol.(names_x), [(; zip(Symbol.(decode_x[x].classes), [Dict("$x_val|$y_val" => cond_count[x][x_v, y_v] for (y_v,y_val) in enumerate(decode_y.classes)) for (x_v, x_val) in enumerate(decode_x[x].classes)])...) for x in 1:length(names_x)])...),
+        y=(;zip(Symbol.(decode_y.classes), [[y_prob[y,1], y_prob[y,2]] for y in 1:size(y_prob,1)])...),
         y_counts=(;zip(Symbol.(decode_y.classes), [y_count[y] for y in 1:size(y_prob,1)])...),
     )
 end
